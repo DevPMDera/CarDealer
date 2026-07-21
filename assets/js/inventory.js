addc65d79e84c9653e5b1df7a8e2d4da821a6c8d
DevPMDera <deraiphone2025@gmail.com>
2026-07-21 13:38:08 +0100

    Create inventory.js

---
+++ assets/js/inventory.js
@@ -0,0 +1,20 @@
+async function loadCars() {
+
+    try {
+
+        const response = await databases.listDocuments(
+            DATABASE_ID,
+            CARS_COLLECTION_ID
+        );
+
+        console.log(response.documents);
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+
+}
+
+loadCars();
